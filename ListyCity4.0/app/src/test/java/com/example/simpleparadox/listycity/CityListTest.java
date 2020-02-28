7cb37b2d186d92ce922e1621852b2714f6fa669c
unknown <gauravsekhar98@gmail.com>
2020-02-28 15:02:58 -0700

    Implemented JUnit tests

---
+++ ListyCity4.0/app/src/test/java/com/example/simpleparadox/listycity/CityListTest.java
@@ -0,0 +1,72 @@
+package com.example.simpleparadox.listycity;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class CityListTest {
+
+    private CityList mockCityList() {
+        CityList cityList = new CityList();
+        cityList.add(mockCity());
+        return cityList;
+    }
+    private City mockCity() {
+        return new City("Edmonton", "Alberta");
+    }
+
+    @Test
+    void testAdd() {
+        CityList cityList = mockCityList();
+        assertEquals(1, cityList.getCities().size());
+        City city = new City("Regina", "Saskatchewan");
+        cityList.add(city);
+        assertEquals(2, cityList.getCities().size());
+        assertTrue(cityList.getCities().contains(city));
+    }
+
+    @Test
+    void testAddException() {
+        CityList cityList = mockCityList();
+        City city = new City("Yellowknife", "Northwest Territories");
+        cityList.add(city);
+        assertThrows(IllegalArgumentException.class, () -> {
+            cityList.add(city);
+        });
+    }
+
+    @Test
+    void testGetCities() {
+        CityList cityList = mockCityList();
+        assertEquals(0, mockCity().compareTo(cityList.getCities().get(0)));
+        City city = new City("Charlottetown", "Prince Edward Island");
+        cityList.add(city);
+        assertEquals(0, city.compareTo(cityList.getCities().get(0)));
+        assertEquals(0, mockCity().compareTo(cityList.getCities().get(1)));
+    }
+    @Test
+    void testHasCity() {
+        CityList cityList = mockCityList();
+        assertTrue(cityList.hasCity(mockCity()));
+        assertFalse(cityList.hasCity(new City("Calgary", "Alberta")));
+    }
+
+    @Test
+    void testDeleteCity() throws Exception {
+        CityList cityList = mockCityList();
+        cityList.delete(mockCity());
+        assertFalse(cityList.hasCity(mockCity()));
+        assertThrows(Exception.class, () -> cityList.delete(mockCity()));
+    }
+
+    @Test
+    void testCountCities() throws Exception{
+        CityList cityList = mockCityList();
+        assertEquals(cityList.countCities(), 1);
+        cityList.delete(mockCity());
+        assertEquals(cityList.countCities(), 0);
+        cityList.add(mockCity());
+        cityList.add(new City("Ottawa", "Ontario"));
+        assertEquals(cityList.countCities(), 2);
+    }
+}
